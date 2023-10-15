d8a86ebd4d900acc4e1a973731517dca62d380b8
AzerSD <sioudazer8@gmail.com>
2023-10-15 04:31:17 +0200

    ex00 done

---
+++ ex00/Hello.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    hello.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: asioud <asioud@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/10/15 04:23:55 by asioud            #+#    #+#              #
+#    Updated: 2023/10/15 04:24:59 by asioud           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Germany!")
+ft_set = {"Hello", "Heilbronn!"}
+ft_dict = {"Hello" : "42HN!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
